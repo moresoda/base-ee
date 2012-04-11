6a43b4fb8170e6f03fb06075e88b74446974a169
Jon MacKinnon <jon@moresoda.co.uk>
2012-04-11 11:36:18 +0100

    Added minimee

---
+++ morecms/expressionengine/third_party/minimee/config.php
@@ -0,0 +1,14 @@
+<?php
+if (! defined('MINIMEE_VER'))
+{
+	define('MINIMEE_NAME', 'Minimee');
+	define('MINIMEE_VER',  '1.1.6');
+	define('MINIMEE_AUTHOR',  'John D Wells');
+	define('MINIMEE_DOCS',  'http://johndwells.com/software/minimee');
+	define('MINIMEE_DESC',  'Minimee: minimize & combine your CSS and JS files. For EE2 only.');
+	
+}
+
+$config['name'] = MINIMEE_NAME;
+$config['version'] = MINIMEE_VER;
+$config['nsm_addon_updater']['versions_xml'] = 'http://johndwells.com/software/versions/minimee';
---
+++ morecms/expressionengine/third_party/minimee/language/english/lang.minimee.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang = array(
+	'base_path' => 'Base Path',
+	'note_base_path' => 'Optional, defaults to FCPATH constant, e.g. /path/to/site.com',
+	'base_url' => 'Base URL',
+	'note_base_url' => 'Optional, defaults to Site URL.',
+	'cache_path' => 'Cache Path',
+	'note_cache_path' => 'e.g. /path/to/site.com/cache',
+	'cache_url' => 'Cache URL',
+	'note_cache_url' => 'e.g. http://site.com/cache',
+	'debug' => 'Turn on template debugging?',
+	'note_debug' => 'Turn on to output error debugging messages as HTML comments.',
+	'disable' => 'Disable plugin entirely?',
+	'note_disable' => 'Handy to set to \'Yes\' during development.',
+	'minify_html' => 'Minify HTML?',
+	'note_minify_html' => 'Run the parsed template through an HTML minifier.',
+	'remote_mode' => 'Remote file mode?',
+	'note_remote_mode' => 'Specify how to fetch remote and {stylesheet=} URLs. \'Auto\' mode will try cURL first.',
+	'save' => 'Save Settings',
+	'yes' => 'Yes',
+	'no' => 'No',
+	'auto' => 'Auto',
+	'curl' => 'cURL',
+	'fgc' => 'file_get_contents()',
+	'config_loc_caution_config' => 'Minimee appears to be configured via your config.php file. There is no need to have this extension installed!',
+	'config_loc_caution_global' => 'Minimee appears to be configured via global variables. There is no need to have this extension installed!',
+	'basic_config' => 'Basic Preferences',
+	'advanced_config' => 'Advanced Preferences'
+);
