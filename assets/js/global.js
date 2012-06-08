3b6b13236e870a79a50af669faa25b654f96d6eb
Jon MacKinnon <jon@moresoda.co.uk>
2012-06-08 11:37:50 +0100

    convert files to utf-8

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,7 +5,7 @@ WebFontConfig = {
 		urls: ['/assets/css/font-face.css']
 	},
 	active: function() {
-	
+
 		// Removes the font-face stylesheet once the google appended one is in to prevent duplication of styles in inspector
 		$('link[title=font-face]').remove();
 
