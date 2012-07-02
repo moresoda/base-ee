06c32d7103e55d748881a1a177bc1377f2657452
Jon MacKinnon <jon@moresoda.co.uk>
2012-07-02 14:24:55 +0100

    tweaking

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,9 +6,6 @@ WebFontConfig = {
 	},
 	active: function() {
 
-		// Removes the font-face stylesheet once the google appended one is in to prevent duplication of styles in inspector
-		$('link[title=font-face]').remove();
-
 	}
 };
 (function() {var wf = document.createElement('script');wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';wf.type = 'text/javascript';wf.async = 'true';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(wf, s);})();
