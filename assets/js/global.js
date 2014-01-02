c05f7c44ff84b50a4140000480843f57a1534c3c
Jon MacKinnon <jon@moresoda.co.uk>
2014-01-02 09:49:50 +0000

    updated all files

--- assets/js/global.js
+++ assets/js/global.js
@@ -10,6 +10,9 @@ WebFontConfig = {
 };
 (function() {var wf = document.createElement('script');wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';wf.type = 'text/javascript';wf.async = 'true';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(wf, s);})();
 */
+if($('html').hasClass('ie')) {
+	$('input, textarea').placeholder();
+}
 $(document).ready(function() {
 
 
